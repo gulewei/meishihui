7902976f2971f51bee197970f932b9ecf4b5e6f9
venecy <gu.lewei@qq.com>
2016-10-06 14:59:23 +0800

    header & footer method

--- module/header_com/js/header_com.js
+++ module/header_com/js/header_com.js
@@ -1,5 +1,5 @@
-var com = (function () {
-	// 检测依赖（zepto)
+$(function () {
+		// 检测依赖（zepto)
 	if(!Zepto) {
 		console.error("zepto not found!");
 		return;
@@ -7,30 +7,16 @@ var com = (function () {
 
 	// 获取footer和header节点
 	var
-		root = "../module/header_com/html/";
+		nHeader = $("#headerCom"),
+		nFooter = $("#footerCom"),
+		root = "/module/header_com/html/";
 
 	// 添加DOM结构
-	function headerCom() {
-		var
-			nHeader = $("#headerCom");
+	$.get(root + 'header.html', function(response){
+	  	nHeader.append(response)
+	});
+	$.get(root + 'footer.html', function(response){
+	  	nFooter.append(response)
+	});
 
-		$.get(root + 'header.html', function(response){
-		  	nHeader.append(response)
-		});
-	}
-
-	function footerCom() {
-		var
-			nFooter = $("#footerCom");
-
-		$.get(root + 'footer.html', function(response){
-		  	nFooter.append(response)
-		});
-	}
-
-	return {
-		headerCom: headerCom,
-		footerCom: footerCom
-	}
-})();
-
+})
